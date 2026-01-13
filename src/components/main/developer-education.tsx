3f045b5509cefc95f0e319d9e70c1db70f0ac589
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2026-01-13 22:28:05 +0530

    feat: add DeveloperEducation component to showcase academic background

---
+++ src/components/main/developer-education.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ShellWrapper from "../layouts/shell-wrapper";
+
+const DeveloperEducation = () => {
+  return (
+    <ShellWrapper>
+      <div>
+        <header>
+          <p>Education</p>
+          <h2>Academic Background</h2>
+          <p>
+            My education journey that shaped my foundation in technology and
+            problem solving
+          </p>
+        </header>
+      </div>
+    </ShellWrapper>
+  );
+};
+
+export default DeveloperEducation;
